e132932b111c224fdf7d49082ed332976d16c077
sabitmohamad <mohamadlaitssabit@gmail.com>
2025-11-02 10:15:30 +0700

    Penambahan file dzikir

---
+++ lib/data/dzikir_data.dart
@@ -0,0 +1,44 @@
+List<Map<String, String>> getDzikirList(String category) {
+  switch (category) {
+    case "Dzikir Pagi & Petang":
+      return [
+        {
+          'image': '',
+          'title': '',
+          'arabic': '',
+          'latin': '',
+          'translate': '',
+          'reference': '',
+        },
+        {
+          'image': '',
+          'title': '',
+          'arabic': '',
+          'latin': '',
+          'translate': '',
+          'reference': '',
+        },
+      ];
+    case "Dzikir ":
+      return [
+        {
+          'image': '',
+          'title': '',
+          'arabic': '',
+          'latin': '',
+          'translate': '',
+          'reference': '',
+        },
+        {
+          'image': '',
+          'title': '',
+          'arabic': '',
+          'latin': '',
+          'translate': '',
+          'reference': '',
+        },
+      ];
+    default:
+      return [];
+  }
+}
--- lib/presentation/screen/dashboard_screen.dart
+++ lib/presentation/screen/dashboard_screen.dart
@@ -1,9 +1,14 @@
 import 'package:dashboard_islami/utils/color_constant.dart';
 import 'package:flutter/material.dart';
 
-class DashboardScreen extends StatelessWidget {
+class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
 
+  @override
+  State<DashboardScreen> createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
   @override
   Widget build(BuildContext context) {
     Widget header() {
